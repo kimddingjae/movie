6a6b54be5a6cf1566febd5b62bff53c98531ae43
kimddingjae <mjkim960430@gmail.com>
2022-07-07 00:07:51 +0900

    add navi

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
 import "./App.css";
 
@@ -10,8 +11,9 @@ function App() {
     <Router>
       <Navigation />
       <Routes>
-        <Route path="/" exact={true} element={<Home />} />
+        <Route path="/movie/" exact={true} element={<Home />} />
         <Route path="/about/" element={<About />} />
+        <Route path="/movie/:id" element={<Detail />} />
       </Routes>
     </Router>
   );
